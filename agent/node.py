b113b2acca8aa8b61a2d798bb94a1bcd4fc57310
FJ-RATO <martinho.francisco@ua.pt>
2020-11-12 18:14:42 +0000

    corner deadlock

--- agent/node.py
+++
@@ -1,8 +0,0 @@
-class Node:
-    def __init__(self,position,parent,heuristic,cost):
-        self.position = position #tuple
-        self.parent = None
-        self.heuristic = heuristic
-        self.cost = cost
-        self.evalfunc = heuristic + cost
-        

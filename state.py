5af74650bd35a2a98baa2d652769bbcddbe7b6a2
spiders17@spiders17 <mariana17@ua.pt>
2020-11-27 12:33:12 +0000

    update state

--- state.py
+++ state.py
@@ -7,5 +7,5 @@ class State():
     def __str__(self):
         return f"Caixas: {self.boxes} | keeper: {self.keeper}"
 
-        def __repr__ (self):
-            return str(self)
+    def __repr__ (self):
+        return str(self)

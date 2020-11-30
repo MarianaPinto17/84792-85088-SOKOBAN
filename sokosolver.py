6f5216c904ab21cbf6df35bf5d97061438a55c6e
spiders17@spiders17 <mariana17@ua.pt>
2020-11-30 14:09:04 +0000

    resolver commit desnecessário

--- sokosolver.py
+++ sokosolver.py
@@ -12,53 +12,7 @@ class Sokosolver(SearchDomain):
         self.walls = mapa.filter_tiles([Tiles.WALL])
 
     def actions(self, state):
-        
-        pstate = []
-        keeper_x = state.keeper[0]
-        keeper_y = state.keeper[1]
 
-        #walls = state.walls == self.walls
-        #boxes = state.boxes == self.boxes
-
-        #LEFT
-        if (keeper_x-1, keeper_y not in self.walls):
-            if (keeper_x-1,keeper_y not in self.boxes):
-                pstate.append("Left")
-            else:
-                if (keeper_x-2,keeper_y not in self.boxes):
-                    if (keeper_x-2,keeper_y not in self.walls):
-                        pstate.append("Left")
-
-        #RIGHT
-        if (keeper_x+1, keeper_y not in self.walls):
-            if (keeper_x+1,keeper_y not in self.boxes):
-                pstate.append("Right")
-            else:
-                if (keeper_x+2,keeper_y not in self.boxes):
-                    if (keeper_x+2,keeper_y not in self.walls):
-                        pstate.append("Right")
-
-        #UP
-        if (keeper_x, keeper_y-1 not in self.walls):
-            if (keeper_x,keeper_y-1 not in self.boxes):
-                pstate.append("Right")
-            else:
-                if (keeper_x,keeper_y-2 not in self.boxes):
-                    if (keeper_x,keeper_y-2 not in self.walls):
-                        pstate.append("Right")
-
-        #DOWN
-        if (keeper_x, keeper_y+1 not in self.walls):
-            if (keeper_x,keeper_y+1 not in self.boxes):
-                pstate.append("Right")
-            else:
-                if (keeper_x,keeper_y-2 not in self.boxes):
-                    if (keeper_x,keeper_y-2 not in self.walls):
-                        pstate.append("Right")
-
-        return pstate
-
-        '''
         #keeper neighborhood
         up = (state.keeper[0] ,state.keeper[1] - 1)
         down = (state.keeper[0], state.keeper[1] + 1)
@@ -76,28 +30,11 @@ class Sokosolver(SearchDomain):
         print(f"Next states: {pstate}")
 
         return pstate
-        '''
+    
     #PROBLEMA É AQUI!!!!!!!!!!!!
     # resultado de uma accao num estado, ou seja, o estado seguinte
     def result(self, state, action):
-        current_state = state 
-        keeper = list(current_state.keeper)
-        boxes = list(current_state.boxes)
-        
-        assert action in self.actions(state)
-        
-        x,y = find_move(action)
 
-        for idx, box in enumerate(boxes):
-            if (box == (keeper[0],keeper[1])):
-                box_x = box[0] + x
-                box_y = box[1] + y
-                boxes[idx] = (box_x,box_y)
-                
-        new_state = State(state.boxes,keeper)
-
-        return new_state
-    '''
         new_state = State(state.boxes,action)
         # if action is go to box I push the box
         if action in state.boxes: #se a açao está em state.boxes
@@ -117,13 +54,8 @@ class Sokosolver(SearchDomain):
         
             #print(f"oldbox{state.boxes}")
             print(f"newbox{new_state.boxes}")
-<<<<<<< HEAD
-    '''
-=======
 
         return new_state
-
->>>>>>> b36d3c8a8c9ed6d350de52b5303cf344adfa35e3
 
     # custo de uma accao num estado
     def cost(self, state, action):
@@ -136,16 +68,3 @@ class Sokosolver(SearchDomain):
     # test if the given "goal" is satisfied in "state"
     def satisfies(self, state, goal):
         return set(state.boxes)== set(goal)
-
-def find_move(action):
-        x = 0
-        y = 0
-        if (action == "Up"):
-            y -= 1
-        elif (action == "Down"):
-            y += 1
-        elif (action == "Left"):
-            x -= 1
-        elif (action == "Right"):
-            x += 1
-        return x,y
